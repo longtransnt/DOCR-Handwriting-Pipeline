17d86b0fc692d8b31fa2880b57c88b195de8a020
chrisidenbui <38187181+chrisidenbui@users.noreply.github.com>
2022-04-24 11:25:59 +0700

    update default input,output,annotated path

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import argparse
 
 #----------------------------Parse req. arguments------------------------------#
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--input", type=str, default="Input/",
+ap.add_argument("-i", "--input", type=str, default="/mnt/d/DOCR/OUCRU-Handwriting-Pipeline/Input",
 	help="path to (optional) input images file")
-ap.add_argument("-o", "--output", type=str, default="Output/",
+ap.add_argument("-o", "--output", type=str, default="/mnt/d/DOCR/OUCRU-Handwriting-Pipeline/Output",
 	help="path to (optional) output images file")
-ap.add_argument("-oa", "--output_annotated", type=str, default="Annotated_Output/",
+ap.add_argument("-oa", "--output_annotated", type=str, default="/mnt/d/DOCR/OUCRU-Handwriting-Pipeline/Annotated_Output",
 help="path to (optional) annotated output images file")
 ap.add_argument("-rt", "--retrain", type=str, default="False",
 	help="whether or not model should be retrained")
