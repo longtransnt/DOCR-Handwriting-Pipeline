1c9660a1468acc0a46ae05729eeee0d61c035a1f
chrisidenbui <38187181+chrisidenbui@users.noreply.github.com>
2022-05-29 10:29:24 +0700

    "minor changes for demo"

---
+++ Misc/constant.py
@@ -0,0 +1,6 @@
+DEFAULT_PATH = "/mnt/d/DOCR/OUCRU-Handwriting-Pipeline"
+PAPERDETCTION_FOLDER_SUFFIX = '/PaperDetection'
+PREPROCESSING_FOLDER_SUFFIX = '/Preprocessing'
+INPUT_SUFFIX = '/Input'
+OUTPUT_SUFFIX = '/Output'
+ANNOTATED_OUTPUT_SUFFIX = '/Annotated_Output'
