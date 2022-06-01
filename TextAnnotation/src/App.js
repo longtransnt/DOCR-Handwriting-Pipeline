c5497a79a4da88858636c4083a369bcf494c48b2
Long Tran <s3755614@rmit.edu.vn>
2022-06-01 11:42:36 +0700

    Update Text Annotate App

--- TextAnnotation/src/App.js
+++ TextAnnotation/src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import Form from 'react-bootstrap/Form'
 import React, { useState } from "react";
 import Button from 'react-bootstrap/Button'
-import { v4 as uuidv4 } from 'uuid';
-
+import ListGroup from 'react-bootstrap/ListGroup'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Stack from 'react-bootstrap/Stack'
 let imageList = {};
 
 // Import all images in data folder
@@ -20,20 +22,41 @@ function App() {
   const [currImage, setCurrImage] = useState(0);
   const [annotation, setAnnotation] = useState('');
   const [annotationList, setAnnotationList] = React.useState([]);
+  const [updateState, setUpdateState] = useState(0);
 
   const handleAdd = () => {
-    // Put function to save annotation here
-    const newList = annotationList.concat(imageList[currImage] + " " + annotation + "\n")
-    setAnnotationList(newList);
+    console.log(updateState);
+    if (updateState === 1) {
+      annotationList[currImage] = imageList[currImage] + "\t" + annotation + "\n";
+      setUpdateState(0);
+    }
+    else {
+      // Put function to save new annotation here
+      const newList = annotationList.concat(imageList[currImage] + "\t" + annotation + "\n")
+      setAnnotationList(newList);
+    }
 
     // Move to next image
     setAnnotation('')
     setCurrImage(currImage + 1);
   }
 
+  const handleListClick = (id) => {
+     // Move to this image
+     setCurrImage(id);
+     setAnnotation("");
+     setUpdateState(1);
+  }
+
   function WriteToFile() {
     const element = document.createElement("a");
-    const file = new Blob(annotationList, {
+    console.log(annotationList);
+    
+    var data = annotationList.filter(function( element ) {
+      return element !== undefined;
+    });
+
+    const file = new Blob(data, {
       type: "text/plain"
     });
     element.href = URL.createObjectURL(file);
@@ -47,24 +70,45 @@ function App() {
       <header className="App-header">
       <p>Image Annotation</p>
     
-      {/* Displaying Image */}
-      <img src= {images[currImage]} />
-
-      {/* Form for text input */}
-      <Form onSubmit={handleAdd}>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <br/>
-        <Form.Label>Enter annotation</Form.Label>
-        <br/>
-        <Form.Control as="textarea" rows={4} value={annotation} onChange={(e) => {setAnnotation(e.target.value)}}/>
-      </Form.Group>
-      </Form>
-      <p>{annotation}</p>
-      <br/>
-      {/* Button to save annotation */}
-      <Button variant="primary" onClick={handleAdd}>Save this annotation</Button>{' '}
-      <Button variant="primary" onClick={WriteToFile}>Download all annotation</Button>{' '}
-
+      <Container>
+        <Row xs={1} md={2}>
+          <Col>
+            {/* Displaying Image */}
+            <p>Current Image</p>
+            <Stack gap={4} className="col-md-11 mx-auto">
+              <img id= "currentImage" src={images[currImage]} />
+              <p>Annotated Text: {annotationList[currImage] !== undefined ? annotationList[currImage]: "None"}</p>
+              <div>
+                <Form onSubmit={handleAdd}>
+                  <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Control as="textarea" rows={4} placeholder="Enter your annotation here." value={annotation} onChange={(e) => {setAnnotation(e.target.value)}}/>
+                  </Form.Group>
+                </Form>
+                <Button variant="primary" onClick={handleAdd}>Save this annotation</Button>{' '}
+                <Button variant="primary" onClick={WriteToFile}>Download all annotation</Button>{' '}
+              </div>
+            </Stack>
+          </Col>
+          <Col>
+            <ListGroup>
+              <div id="image-list">
+                {imageList.map((im, index) => (
+                    <ListGroup.Item 
+                    id={"image_" + index} 
+                    key={index} 
+                    value={index}
+                    variant={annotationList[index] !== undefined ? "success" : "danger"} 
+                    onClick={(e) => {handleListClick(index)}
+                }>
+                      <img src= {images[index]} />
+                      {im}
+                    </ListGroup.Item>
+                ))}
+              </div>
+            </ListGroup>
+          </Col>
+        </Row>
+      </Container>
       </header>
     </div>
   );
