99272790a4574cf3e9669b00888ff8d92e30e96a
Nhan <s3687637@rmit.edu.vn>
2022-06-08 21:08:03 +0700

    add verification

--- TextAnnotation/src/App.js
+++ TextAnnotation/src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import Form from 'react-bootstrap/Form'
 import React, { useState } from "react";
-import Button from 'react-bootstrap/Button'
 import ListGroup from 'react-bootstrap/ListGroup'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Stack from 'react-bootstrap/Stack'
+import ImageUpload from './components/ImageUpload';
+import { ToastContainer, toast } from 'react-toastify';
+import { Scrollbars } from 'react-custom-scrollbars'
+import 'react-toastify/dist/ReactToastify.css';
 let imageList = {};
+let verified = {};
 
 // Import all images in data folder
 function importAll(r) {
@@ -16,6 +20,26 @@ function importAll(r) {
 }
 
 const images = importAll(require.context('./../data', false, /\.(png|jpe?g|svg)$/));
+
+const notiSaving = () => toast.warn('Please input annotation before saving!', {
+  position: "top-right",
+  autoClose: 2000,
+  hideProgressBar: false,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+  progress: undefined,
+});
+
+const notiDownload = () => toast.warn('Required at least 1 annotation to download!', {
+  position: "top-right",
+  autoClose: 2000,
+  hideProgressBar: false,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+  progress: undefined,
+});
 
 // Main application
 function App() {
@@ -27,19 +51,24 @@ function App() {
 
   const handleAdd = () => {
     console.log(updateState);
-    if (updateState === 1) {
-      annotationList[currImage] = imageList[currImage] + "\t" + annotation + "\n";
-      setUpdateState(0);
-    }
-    else {
-      // Put function to save new annotation here
-      const newList = annotationList.concat(imageList[currImage] + "\t" + annotation + "\n")
-      setAnnotationList(newList);
-    }
-
+    if (annotation !==  '') {
+      if (updateState === 1 ) {
+        annotationList[currImage] = imageList[currImage] + "\t" + annotation + "\n";
+        verified[currImage] = checked;
+        setUpdateState(0);
+      }
+      else {
+        // Put function to save new annotation here
+        const newList = annotationList.concat(imageList[currImage] + "\t" + annotation + "\n")
+        setAnnotationList(newList);
+      }
     // Move to next image
     setAnnotation('')
     setCurrImage(currImage + 1);
+    } else {
+      setUpdateState(0);
+      notiSaving(); // not allow to save if no annotation
+    }
   }
 
   const handleListClick = (id) => {
@@ -47,33 +76,35 @@ function App() {
      setCurrImage(id);
      setAnnotation("");
      setUpdateState(1);
+     setChecked(false);
   }
 
   function WriteToFile() {
     const element = document.createElement("a");
     console.log(annotationList);
-    
-    var data = annotationList.filter(function( element ) {
-      return element !== undefined;
-    });
-
-    const file = new Blob(data, {
-      type: "text/plain"
-    });
-    element.href = URL.createObjectURL(file);
-    element.download = "annotation.txt";
-    document.body.appendChild(element);
-    element.click();
+    if (annotationList.length === 0) {
+      notiDownload()
+    } else {
+      var data = annotationList.filter(function( element ) {
+        return (element !== undefined && verified[annotationList.indexOf(element)]);
+      });
+      const file = new Blob(data, {
+        type: "text/plain"
+      });
+      element.href = URL.createObjectURL(file);
+      element.download = "annotation.txt";
+      document.body.appendChild(element);
+      element.click();
+    }
   }
 
-  const handleChange = () => {
-    setChecked(!checked);
+  const handleChecked = () => {
+    setChecked(!checked)
   };
 
   return (
     <div className="App">
       <div className="App-header">
-      <Button className='upload-btn'>Upload</Button>
       <p>Image Annotation</p>
     
       <Container>
@@ -83,54 +114,77 @@ function App() {
             <p>Current Image</p>
             <Stack gap={4} className="col-md-11 mx-auto">
               <img id= "currentImage" src={images[currImage]} />
-              <p>Annotated Text: {annotationList[currImage] !== undefined ? annotationList[currImage] : "None"}</p>
-              <div>
-                <Form onSubmit={handleAdd}>
-                  <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Control 
-                    as="textarea" 
-                    rows={4} 
-                    placeholder="Enter your annotation here." 
-                    style={{border: 'none', height: '20vh'}}
-                    value={annotation} 
-                    onChange={(e) => {setAnnotation(e.target.value)}}
+              <p style={{fontSize: '22px'}}>Annotated Text: 
+                <span style={{fontSize: '18px', paddingLeft: '5px'}}>
+                  {annotationList[currImage] !== undefined ? annotationList[currImage].split(imageList[currImage] + "\t") : annotation !== '' ? annotation : "None"}
+                </span>
+              </p>
+              <Form onSubmit={handleAdd}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Control 
+                  as="textarea" 
+                  rows={4} 
+                  placeholder="Enter your annotation here." 
+                  style={{border: 'none', height: '20vh'}}
+                  value={annotation} 
+                  onChange={(e) => {setAnnotation(e.target.value)}}
+                  />
+                  <div style={{fontSize: '1rem', color: '#005477', fontWeight: '500', float: 'right'}}>
+                    <input 
+                    id='checkbox3'
+                    style={{margin: '5px 5px 0 0', color: '#005477', fontWeight: '500', cursor: 'pointer'}} 
+                    type="checkbox"
+                    checked={checked} onChange={handleChecked}
                     />
-                    <div style={{fontSize: '1rem', color: '#005477', fontWeight: '500'}}>
-                      <input 
-                      style={{marginRight: '5px', color: '#005477', fontWeight: '500'}} 
-                      type="checkbox"
-                      checked={checked} onChange={handleChange}
-                      />
-                      Verified by OUCRU
-                    </div>
-                  </Form.Group>
-                </Form>
-                <Button variant="primary" onClick={handleAdd}>Save this annotation</Button>{' '}
-                <Button variant="primary" onClick={WriteToFile}>Download all annotation</Button>{' '}
-              </div>
+                    <label htmlFor="verified" onClick={handleChecked} style={{cursor: 'pointer'}}>Verified by OUCRU</label>
+                  </div>
+                  <div style={{fontSize: '1rem', color: '#005477', float: 'left'}}>
+                    <label>% Confidence</label>
+                    <input type="text"
+                      style={{margin: '5px 0 0 5px', color: '#005477', width: '3vw', border: 'none'}}
+                    />
+                  </div>
+                </Form.Group>
+              </Form>
             </Stack>
           </Col>
           <Col>
-            <ListGroup>
+            <Scrollbars>
               <div id="image-list">
                 {imageList.map((im, index) => (
                     <ListGroup.Item 
-                    id={"image_" + index} 
-                    key={index} 
-                    value={index}
-                    variant={annotationList[index] !== undefined ? "success" : "danger"} 
-                    onClick={(e) => {handleListClick(index)}
-                }>
+                      id={"image_" + index} 
+                      key={index} 
+                      value={index}
+                      variant={
+                        annotationList[index] == undefined ? "danger" : 
+                        verified[index] === false ? "warning" : "success"
+                      } 
+                      style={{cursor: 'pointer'}}
+                      onClick={(e) => {handleListClick(index)}}
+                    >
                       <img src= {images[index]} />
                       {im}
                     </ListGroup.Item>
                 ))}
               </div>
-            </ListGroup>
+            </Scrollbars>
           </Col>
         </Row>
       </Container>
+      <Row style={{marginTop: '7rem'}}>
+          <Col>
+            <div style={{float: 'left'}}>
+              <ImageUpload />
+            </div>
+            <div style={{float: 'right'}}>
+              <button className='save-btn' onClick={handleAdd}>Save the annotation</button>{' '}
+              <button className='download-btn' onClick={WriteToFile}>Export all annotation</button>{' '}
+            </div>
+          </Col>
+        </Row>
       </div>
+      <ToastContainer style={{width: '20vw'}} />
     </div>
   );
 }
