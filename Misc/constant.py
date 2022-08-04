e3d4a112ffd4a03cec8b2fb1ee6ffb00b95f52fe
chrisidenbui <38187181+chrisidenbui@users.noreply.github.com>
2022-08-04 15:23:19 +0700

    cleanup and add steps to TD

--- Misc/constant.py
+++ Misc/constant.py
@@ -1,7 +1,7 @@
 DEFAULT_PATH = "/mnt/d/DOCR/OUCRU-Handwriting-Pipeline"
 PAPERDETCTION_FOLDER_SUFFIX = '/PaperDetection'
 PREPROCESSING_FOLDER_SUFFIX = '/Preprocessing'
-TEXTDETECRION_FOLDER_SUFFIX = '/TextDetection'
+TEXTDETECTION_FOLDER_SUFFIX = '/TextDetection'
 INPUT_SUFFIX = '/Input'
 OUTPUT_SUFFIX = '/Output'
 ANNOTATED_OUTPUT_SUFFIX = '/Annotated_Output'
--- PaperDetection/PaperDetection.py
+++ PaperDetection/PaperDetection.py
@@ -20,11 +20,13 @@ import shutil
 
 MODULE_NAME = "PaperDetection"
 
+
 class NumpyArrayEncoder(JSONEncoder):
     def default(self, obj):
         if isinstance(obj, np.ndarray):
             return obj.tolist()
         return JSONEncoder.default(self, obj)
+
 
 class MaskCRNN(object):
     cfg = get_cfg()
@@ -33,13 +35,14 @@ class MaskCRNN(object):
 
     def __init__(self, output_path, annotated_output_path):
         self.output_path = output_path
-        self.output_annotation_path = annotated_output_path 
-        
+        self.output_annotation_path = annotated_output_path
+
         self.cfg.merge_from_file(
             "./PaperDetection/configs/COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"
         )
         self.cfg.DATALOADER.NUM_WORKERS = 2
-        self.cfg.MODEL.WEIGHTS = "./PaperDetection/weights/model_final_0205.pth"  # initialize from model zoo
+        # initialize from model zoo
+        self.cfg.MODEL.WEIGHTS = "./PaperDetection/weights/model_final_0205.pth"
         self.cfg.SOLVER.IMS_PER_BATCH = 2
         self.cfg.SOLVER.MAX_ITER = (
             300
@@ -48,9 +51,11 @@ class MaskCRNN(object):
             128
         )  # faster, and good enough for this toy dataset
         self.cfg.OUTPUT_DIR = './output/'
-        self.cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # 3 classes (data, fig, hazelnut)
+        # 3 classes (data, fig, hazelnut)
+        self.cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1
         # self.cfg.MODEL.WEIGHTS = os.path.join(self.cfg.OUTPUT_DIR, "model_final.pth")
-        self.cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.8  # set the testing threshold for this model
+        # set the testing threshold for this model
+        self.cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.8
         # self.cfg.DATASETS.TEST = ("bills",)
         self.predictor = DefaultPredictor(self.cfg)
 
@@ -68,33 +73,32 @@ class MaskCRNN(object):
 
             # Take the first polygons and find the minimum bounding box (there is only 1 class)
             # This 'box' varriable hold the 4 corners of the predicted text area in our model - Long
-            min_rect = cv2.minAreaRect(polygons.points[0])  
+            min_rect = cv2.minAreaRect(polygons.points[0])
             box = cv2.boxPoints(min_rect)
             box = np.intp(box)
 
             # Sort the array else the order will be fucked
             box = box[box[:, 1].argsort()]
             if (box[0][0] > box[1][0]):
-                 box[[0,1]] = box[[1,0]]
+                box[[0, 1]] = box[[1, 0]]
 
             if(box[2][0] < box[3][0]):
-                box[[2,3]] = box[[3,2]]
-            
+                box[[2, 3]] = box[[3, 2]]
+
             # box = self.alterPredictionBoundingBox(box, 25)
             # print(box)
 
             v = Visualizer(im[:, :, ::-1],
-                        scale=1,
-                        instance_mode=ColorMode.IMAGE_BW  # remove the colors of unsegmented pixels
-                        )
-            
+                           scale=1,
+                           instance_mode=ColorMode.IMAGE_BW  # remove the colors of unsegmented pixels
+                           )
+
             # cv2.drawContours(im,[box],0,(0,0,255),2) #Draw Box of Predicted ROI
 
-            
             roi_paper_img = four_point_transform(im, box)
             stuff = self.cropDarkEdges(roi_paper_img)
             v = v.draw_instance_predictions(outputs["instances"].to("cpu"))
-            self.convertToAnnotation( name=name, data=data, box=box)
+            self.convertToAnnotation(name=name, data=data, box=box)
             # Exporting image with prediction contours
             # cv2.imwrite(self.output_annotation_path + "/" + "out_" + name + ".jpg", roi_paper_img)
             return stuff
@@ -116,37 +120,35 @@ class MaskCRNN(object):
             min_rect = cv2.minAreaRect(polygons.points[0])
             box = cv2.boxPoints(min_rect)
             box = np.intp(box)
-         
-
 
             v = Visualizer(im[:, :, ::-1],
-                        scale=1,
-                        instance_mode=ColorMode.IMAGE_BW  # remove the colors of unsegmented pixels
-                        )
+                           scale=1,
+                           instance_mode=ColorMode.IMAGE_BW  # remove the colors of unsegmented pixels
+                           )
 
             roi_bill_img = four_point_transform(im, box)
             v = v.draw_instance_predictions(outputs["instances"].to("cpu"))
             # + ("/" if self.output_path[-1] != '/' else "")
-            image_name = self.output_path + "/Alter/"  + name + "pd" + ".jpg"
-            
-            cv2.imwrite(image_name , roi_bill_img)
+            # image_name = self.output_path + name + "pd" + ".jpg"
+
+            # cv2.imwrite(image_name, roi_bill_img)
 
             # Sort the array else the order will be fucked
             box = box[box[:, 1].argsort()]
             if (box[0][0] > box[1][0]):
-                 box[[0,1]] = box[[1,0]]
+                box[[0, 1]] = box[[1, 0]]
 
             if(box[2][0] < box[3][0]):
-                box[[2,3]] = box[[3,2]]
-            
-            box = self.alterPredictionBoundingBox(box, 1,1)
+                box[[2, 3]] = box[[3, 2]]
+
+            box = self.alterPredictionBoundingBox(box, 1, 1)
 
             roi_bill_img = four_point_transform(im, box)
             return_img = self.cropDarkEdges(roi_bill_img)
-            image_name = self.output_path + "/Alter/"  + name + "pd" + "_alter" + ".jpg"
-            
-            cv2.imwrite(image_name , return_img)
-            return image_name
+            image_name = self.output_path + "/" + name + "pd" + ".jpg"
+
+            cv2.imwrite(image_name, return_img)
+            return return_img, image_name
 
     def convertToAnnotation(self, name, data, box):
         annotatation = {
@@ -183,14 +185,11 @@ class MaskCRNN(object):
         }
 
         filename = self.output_annotation_path + "/" + name + '.json'
-        print("inside path", filename)
         # Using a JSON string
         with open(filename, 'w') as outfile:
             json.dump(annotatation, outfile)
 
-    def alterPredictionBoundingBox(self, box, lenghthenYRate = 1, lenghthenXRate  = 1):
-        print ("before box:")
-        print(box)
+    def alterPredictionBoundingBox(self, box, lenghthenYRate=1, lenghthenXRate=1):
         yList = [box[0][1], box[1][1], box[2][1], box[3][1]]
         xList = [box[0][0], box[1][0], box[2][0], box[3][0]]
 
@@ -201,27 +200,24 @@ class MaskCRNN(object):
         # Get Left and right X of the images
         currentRightXValue = max(xList)
         currentLeftXValue = min(xList)
-       
+
         h_x = (currentRightXValue - currentLeftXValue)
         h_y = (currentBottomYValue - currentTopYValue)
 
-        adjustedYHeight =  h_y * (lenghthenYRate/100)
+        adjustedYHeight = h_y * (lenghthenYRate/100)
         adjustedXWidth = h_x * (lenghthenXRate/100)
 
-        # Adjust the bottom value 
-        box[2][1] =  currentBottomYValue + adjustedYHeight
-        box[3][1] =  currentBottomYValue + adjustedYHeight
+        # Adjust the bottom value
+        box[2][1] = currentBottomYValue + adjustedYHeight
+        box[3][1] = currentBottomYValue + adjustedYHeight
 
         # Adjust the right value
         box[1][0] = currentRightXValue + adjustedXWidth
         box[2][0] = currentRightXValue + adjustedXWidth
 
-        #adjust the left value
+        # adjust the left value
         box[0][0] = currentLeftXValue - adjustedXWidth
         box[3][0] = currentLeftXValue - adjustedXWidth
-
-        print("after box: ")
-        print(box)
         return box
 
     def cropDarkEdges(self, image):
@@ -230,12 +226,12 @@ class MaskCRNN(object):
 
     def correctionFeature(self):
         # ROI Moust Drag Code
-            roi = cv2.selectROI(im)
-            #print(roi)
+        roi = cv2.selectROI(im)
+        # print(roi)
 
-            if roi == (0,0,0,0):
-                print("here")
-                roi_bill_img = four_point_transform(im, box)
-            else:
-                 roi_bill_img = im[int(roi[1]):int(roi[1]+roi[3]), int(roi[0]):int(roi[0]+roi[2])]
-
+        if roi == (0, 0, 0, 0):
+            print("here")
+            roi_bill_img = four_point_transform(im, box)
+        else:
+            roi_bill_img = im[int(roi[1]):int(
+                roi[1]+roi[3]), int(roi[0]):int(roi[0]+roi[2])]
